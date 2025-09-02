a5d198b01b63ffcfbc180b1d8ad7cbee4af9597b
hmf-jc <165405431+hmf-jc@users.noreply.github.com>
2025-09-02 11:44:39 -0400

    add logging

--- api/app/main.py
+++ api/app/main.py
@@ -2,6 +2,15 @@ from fastapi import FastAPI, Request, Response
 import uvicorn
 import psycopg2
 import random
+import logging
+
+logging.basicConfig(
+    filename='./logs/my_application.log',
+    filemode='a',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
  
 first = ["Bob", "Fred", "Jim", "Leopold"]
 last = ["Ross", "Rogers", "Henson", "Stokowski"]
@@ -15,6 +24,8 @@ conn = psycopg2.connect(
             host="demo_db", # demo_db
             port=5432
         )
+
+logging.info("Database connetion established")
 
 # DB Setup 
 with conn.cursor() as crs:
@@ -31,11 +42,14 @@ with conn.cursor() as crs:
     
     conn.commit()
 
+    logging.info("Database table test initialized")
+
 @app.get('/')
 def read_root(req: Request, resp: Response):
     with conn.cursor() as crs:
         crs.execute("select * from public.test;")
         data = crs.fetchall()
+        logging.info("User records retrieved")
     return set(data)
 
 @app.post('/')
@@ -48,6 +62,8 @@ def read_root(req: Request, resp: Response):
             """, (user, random.randint(1,1_000)))
         
         conn.commit()
+        
+        logging.info(f"User record add: {user}")
     return user
 
 
